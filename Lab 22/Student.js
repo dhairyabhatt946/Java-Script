68fe1e9806a6cf692753b77992c8c75670171359
Dhairya Bhatt <143011610+dhairyabhatt946@users.noreply.github.com>
2024-08-14 09:20:41 +0530

    Add files via upload

---
+++ Lab 22/Student.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    enrollment: Number,
+    name: String,
+    age: Number,
+    branch: String,
+    semester: Number
+});
+
+module.exports = mongoose.model('Student', schema);
